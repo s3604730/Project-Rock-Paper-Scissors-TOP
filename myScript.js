bbd4cd08af300e0a568b845076920994cd8f63d4
Tony Tran <tonytranme0@hotmail.com>
2022-04-20 21:50:22 +1000

    Added condition for equality of answer

--- myScript.js
+++ myScript.js
@@ -1,3 +1,20 @@
+
+
+function playRound(playerSelection, computerSelection){
+   // var getPlayerAnswer = window.prompt("Enter either Rock, Paper or Scissors");
+    playerSelectionAnswer = playerSelection.lowercase();
+    computerSelectionAnswer = computerSelection.lowercase();
+
+    if (playerSelectionAnswer == computerSelectionAnswer){
+        return "Draw!";
+    }
+
+    
+
+}
+
+
+
 function getRockPaperScissorValue(){
     return Math.floor(Math.random() * 3);
 }
@@ -6,7 +23,10 @@ function computerPlay(){
    const RockPaperScissorArray = ["Rock", "Paper", "Scissors"];
     answer = getRockPaperScissorValue();
     console.log(answer);
-    console.log(RockPaperScissorArray[answer])
+    getAnswer=RockPaperScissorArray[answer];
+    console.log(getAnswer);
+    return(getAnswer);
+  
 
 }
 
